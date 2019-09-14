972cccc55fa11f8d0517991822c5353331b81e66
haozhang <haozhang>
2019-09-14 17:16:42 -0400

    1[CHG] button highlight

--- Vic.SportsStore/Vic.SportsStore.UnitTests/UnitTest1.cs
+++ Vic.SportsStore/Vic.SportsStore.UnitTests/UnitTest1.cs
@@ -87,5 +87,29 @@ namespace Vic.SportsStore.UnitTests
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
         }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductId = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductId = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductId = 4, Name = "P4", Category = "Oranges"},
+            });
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+            // Act = get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
     }
 }
--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/NavController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/NavController.cs
@@ -14,8 +14,10 @@ namespace Vic.SportsStore.WebApp.Controllers
         {
             repository = repo;
         }
-        public PartialViewResult Menu()
+        public PartialViewResult Menu(string category = null)
         {
+            ViewBag.SelectedCategory = category;
+
             IEnumerable<string> categories = repository
             .Products
             .Select(x => x.Category)
