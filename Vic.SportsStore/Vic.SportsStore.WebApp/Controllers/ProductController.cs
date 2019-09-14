993be6574724bf00f7a5c487602c44e84154d71d
haozhang <haozhang>
2019-09-14 17:24:47 -0400

    1[CHG] fix page number bug

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace Vic.SportsStore.WebApp.Controllers
         //}
 
         public ViewResult List(string category, int page = 1)
-        {
+{
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = ProductsRepository
@@ -33,7 +33,10 @@ namespace Vic.SportsStore.WebApp.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = ProductsRepository.Products.Count()
+                    TotalItems = ProductsRepository
+                    .Products
+                    .Where(p => category == null || p.Category == category)
+                    .Count()
                 },
 
                 CurrentCategory = category
