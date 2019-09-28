5a73da35f2712b579accce355932f7297436632a
haozhang <haozhang>
2019-09-28 11:20:13 -0400

    1[CHG] update UnitTest1

--- Vic.SportsStore/Vic.SportsStore.Domain/Entities/Cart.cs
+++ Vic.SportsStore/Vic.SportsStore.Domain/Entities/Cart.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Vic.SportsStore.Domain.Abstract;
 
 
 namespace Vic.SportsStore.Domain.Entities
