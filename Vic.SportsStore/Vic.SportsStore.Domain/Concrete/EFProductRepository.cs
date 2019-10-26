d38345b12bcbf3927470831585833dd5d8fa58f2
haozhang <haozhang>
2019-10-26 16:35:32 -0400

    1[CHG] add SaveProduct function in AdminController.cs

--- Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EFProductRepository.cs
+++ Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using Vic.SportsStore.Domain.Abstract;
@@ -17,6 +18,28 @@ namespace Vic.SportsStore.Domain.Concrete
             {
                 return Context.Products;
             }
+        }
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductId == 0)
+            {
+                Context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = Context
+                    .Products
+                    .Find(product.ProductId);
+
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            Context.SaveChanges();
         }
     }
 }
