0ca84999ef8187fa76c1ee40ed3e4a52765f12f7
haozhang <haozhang>
2019-11-02 16:23:28 -0400

    1[CHG] update packages

---
+++ Vic.SportsStore/Vic.SportsStore.Domain/Entities/LoginUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vic.SportsStore.Domain.Entities
+{
+    public class LoginUser
+    {
+        public int LoginUserId { get; set; }
+        public string Username { get; set; }
+
+    }
+}
