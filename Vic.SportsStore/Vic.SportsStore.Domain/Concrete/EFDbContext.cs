b1179545f934d975daef04fa6942bd06fd84c480
haozhang <haozhang>
2019-11-02 16:27:34 -0400

    1[CHG]

--- Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EFDbContext.cs
+++ Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EFDbContext.cs
@@ -11,7 +11,5 @@ namespace Vic.SportsStore.Domain.Concrete
     public class EFDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
-
-        public DbSet<LoginUser> LoginUsers { get; set; }
     }
 }
