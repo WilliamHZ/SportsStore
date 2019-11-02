a03e39932e93ac8d5724be5d9ac4ef5bc73d91af
haozhang <haozhang>
2019-11-02 15:04:47 -0400

    1[NEW] add IAuthProvider.cs and FormsAuthProvider.cs, update AdminController.cs and Web.config

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vic.SportsStore.WebApp.Concrete
+{
+    public class FormsAuthProvider
+    {
+    }
+}
